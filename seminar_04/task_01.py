8724bd371f083e801365682dc33f1d3aef8490d0
Alexey Chernyshov <111124292+lexxfin@users.noreply.github.com>
2022-11-03 13:46:31 +0300

    ok

--- seminar_04/task_01.py
+++ seminar_04/task_01.py
@@ -1,7 +1,5 @@
 # Вычислить число c заданной точностью d
 # Пример: при d = 0.001, π = 3.142 10^(-1) ≤ d ≤10^(-10)
 
-from math import pi
-
-d = input('d = ').strip()
-print('d =', d, 'π =', round(pi, len(d.split(".")[1])))
+d, num = input('d = ').strip(), float(input('num = '))
+print(f'{d = }, num = {round(num, len(d.split(".")[1]))}')
