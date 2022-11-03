d0fb3900bd55545bcf89cd54639e8d5123145a32
Alexey Chernyshov <111124292+lexxfin@users.noreply.github.com>
2022-11-03 13:28:32 +0300

    Apply suggestions from code review
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Seminar_02/task_02.py
+++ Seminar_02/task_02.py
@@ -4,6 +4,6 @@
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
 N = int(input('N: '))
-fact = [1]
-[fact.append(fact[i] * (i + 2)) for i in range(N - 1)]
+last = 1
+fact = [last := last * i for i in range(1, N + 1)]
 print(f'{N=} -> {fact}')
